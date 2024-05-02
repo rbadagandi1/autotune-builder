0e262303d8debc1257f47b3ea1876a6aecef8c81
Suraj Patil <31805557+patilsuraj767@users.noreply.github.com>
2024-05-02 13:48:21 +0530

    Fixes #RHINENG-9415  - Fix build_deploy.sh (#164)
    * Fixes #RHINENG-9415  - Fix build_deploy.sh
    * exported DOCKER_CONFIG env variable

--- build_deploy.sh
+++ build_deploy.sh
@@ -27,16 +27,27 @@ git clone --branch $AUTOTUNE_GIT_BRANCH https://github.com/kruize/autotune.git $
 pushd $AUTOTUNE_GIT_DIR
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
 
-DOCKER_CONF="$PWD/.docker"
-mkdir -p "$DOCKER_CONF"
-docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
-docker --config="$DOCKER_CONF" login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
-docker --config="$DOCKER_CONF" build -t "${IMAGE_NAME}:${IMAGE_TAG}" -f ${AUTOTUNE_DOCKERFILE} .
-docker --config="$DOCKER_CONF" push "${IMAGE_NAME}:${IMAGE_TAG}"
+# Create tmp dir to store data in during job run (do NOT store in $WORKSPACE)
+export TMP_JOB_DIR=$(mktemp -d -p "$HOME" -t "jenkins-${JOB_NAME}-${BUILD_NUMBER}-XXXXXX")
+echo "job tmp dir location: $TMP_JOB_DIR"
+
+function job_cleanup() {
+    echo "cleaning up job tmp dir: $TMP_JOB_DIR"
+    rm -fr "$TMP_JOB_DIR"
+}
+
+trap job_cleanup EXIT ERR SIGINT SIGTERM
+
+export DOCKER_CONFIG="$TMP_JOB_DIR/.docker"
+mkdir -p "$DOCKER_CONFIG"
+docker login -u="$QUAY_USER" --password-stdin quay.io <<< "$QUAY_TOKEN"
+docker login -u="$RH_REGISTRY_USER" --password-stdin  registry.redhat.io <<< "$RH_REGISTRY_TOKEN"
+docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" -f ${AUTOTUNE_DOCKERFILE} .
+docker push "${IMAGE_NAME}:${IMAGE_TAG}"
 
 for ADDITIONAL_TAG in $ADDITIONAL_TAGS; do
-    docker --config="$DOCKER_CONF" tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:${ADDITIONAL_TAG}"
-    docker --config="$DOCKER_CONF" push "${IMAGE_NAME}:${ADDITIONAL_TAG}"
+    docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:${ADDITIONAL_TAG}"
+    docker push "${IMAGE_NAME}:${ADDITIONAL_TAG}"
 done
 
 popd
